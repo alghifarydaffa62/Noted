e6a96cf3d8470fa8d22276b28f2e7b9fbe79ffe3
M Daffa A <daffa.alghifary62@gmail.com>
2025-06-07 09:40:46 +0700

    tailwindcss, router

---
+++ Noted_ToDoList/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function Home() {
+  return (
+    <>
+      <h1 className='text-4xl text-center font-mono font-semibold'>Noted - To-Do List Website</h1>
+    </>
+  )
+}
+
+export default Home
